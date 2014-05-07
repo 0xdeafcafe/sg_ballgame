cfec52e59af2038d14d6568895e0ff17778b9190
Alex Reed (Xerax) <alexreed@outlook.com>
2014-05-07 01:01:45 +0100

    SendGrid stuff!

---
+++ SendGrid.BallGame.Web/Attributes/Filters/ParseEmailObjectFilter.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using SendGrid.BallGame.Web.Models;
+
+namespace SendGrid.BallGame.Web.Attributes.Filters
+{
+	public class ParseEmailObjectFilter : ActionFilterAttribute
+	{
+		/// <summary>
+		/// Parses the raw form-data of the POST into a snazzy email 
+		/// model, and then sets it in the arguments for the POST function
+		/// to read.
+		/// </summary>
+		/// <param name="actionContext">The HTTP action context of the POST.</param>
+		public override void OnActionExecuting(HttpActionContext actionContext)
+		{
+			var data = HttpContext.Current.Request.Form;
+			var email = new Email
+			{
+				Dkim = data["dkim"],
+				To = data["to"],
+				Html = data["html"],
+				From = data["from"],
+				Text = data["text"],
+				SenderIp = data["sender_ip"],
+				SpamReport = data["spam_report"],
+				Envelope = data["envelope"],
+				Attachments = int.Parse(data["attachments"] ?? "0"),
+				Subject = data["subject"],
+				SpamScore = double.Parse(data["spam_score"] ?? "0.0"),
+				Charsets = data["charsets"],
+				Spf = data["SPF"]
+			};
+
+			actionContext.ActionArguments["email"] = email;
+
+			base.OnActionExecuting(actionContext);
+		}
+	}
+}
---
+++ SendGrid.BallGame.Web/Controllers/InboundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+using SendGrid.BallGame.Web.Attributes.Filters;
+using SendGrid.BallGame.Web.Models;
+
+namespace SendGrid.BallGame.Web.Controllers
+{
+    public class InboundController : ApiController
+    {
+		// POST api/inbound
+		[ValidateInput(false)]
+		[ParseEmailObjectFilter]
+		public ActionResult Post()
+		{
+			if (ActionContext == null || ActionContext.ActionArguments == null || ActionContext.ActionArguments["email"] == null || !ActionContext.ActionArguments.ContainsKey("email"))
+				goto end;
+
+			var email = ActionContext.ActionArguments["email"] as Email;
+			if (email == null) goto end;
+
+			// le logic
+
+		end:
+			return new HttpStatusCodeResult(HttpStatusCode.OK);
+		}
+	}
+}
